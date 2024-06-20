7be2008bffafde48a15e5699c6dfa1f801bd8aa1
shivami-shah <shivamishah@gmail.com>
2024-06-21 02:21:55 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__)
+
+model = pickle.load(open('models/model.pkl', 'rb'))
+scaler = pickle.load(open('models/scaler.pkl', 'rb'))
+
+@app.route('/', methods = ['POST', 'GET'])
+def predict():
+    if request.method == "POST":
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        BUI = float(request.form.get('BUI'))
+        Bejaia = float(request.form.get('Region_Bejaia'))
+        Sidi_Bel = float(request.form.get('Region_Sidi_Bel'))
+        
+        new_data_scaled = scaler.transform([[Rain,FFMC,DMC,ISI,BUI,Bejaia,Sidi_Bel]])
+        result = model.predict(new_data_scaled)
+
+        return render_template('index.html',result=f"The predicted value is {result[0]}")
+
+        
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
